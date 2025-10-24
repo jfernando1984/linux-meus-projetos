91195cf35efd1a73091442c9d86e4b4e4ed19071
José Fernando Torres da Silva <jfts844@gmail.com>
2025-10-24 15:38:14 -0300

    Add files via upload

---
+++ scripts/Jogos e plus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Instalar ferramentas para jogos
+sudo apt install steam-devices -y
+flatpak install flathub com.vysp3r.ProtonPlus
+flatpak install flathub com.steamgriddb.steam-rom-manager
+flatpak install flathub com.steamgriddb.SGDBoop
+flatpak install flathub info.cemu.Cemu
+flatpak install flathub com.usebottles.bottles
+#flatpak install flathub com.heroicgameslauncher.hgl
+#flatpak install flathub dev.lizardbyte.app.Sunshine
+
+# Instalar aplicativos em flatpak
+#flatpak install flathub com.discordapp.Discord
+#flatpak install flathub com.spotify.Client
+#flatpak install flathub tech.feliciano.pocket-casts
+#flatpak install flathub com.obsproject.Studio
+flatpak install flathub io.github.celluloid_player.Celluloid
+#flatpak install flathub org.gnome.Boxes
+#flatpak install flathub com.mattjakeman.ExtensionManager
+#flatpak install flathub com.github.tchx84.Flatseal
+#flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub org.nickvision.tubeconverter
+#flatpak install flathub org.localsend.localsend_app
+#flatpak install flathub page.codeberg.libre_menu_editor.LibreMenuEditor
+flatpak install flathub de.haeckerfelix.Fragments
+flatpak install flathub com.rtosta.zapzap
+#flatpak install flathub com.todoist.Todoist
+#flatpak install flathub com.discordapp.Discord
+#flatpak install flathub io.github.pwr_solaar.solaar
+#flatpak install flathub io.github.brunofin.Cohesion
